b570795bcf119c6999627a1be85f93db67366e40
AlexSmall96 <alex.small739@gmail.com>
2022-11-17 20:57:46 +0000

    "Add eventlistener for dom content loaded"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let goButton = document.getElementById("go-button");
+    goButton.addEventListener('click', runGame);
+})
 
 function runGame(){
-   
+    alert('You clicked go!');
 }
 
 function createSequence(){
-
+    
 }
 
 function compareSequence(){
